bfdc1a75758c10304816f5f37a2515fa0b2b04be
Mian35 <74639641+Mian35@users.noreply.github.com>
2024-05-22 07:09:52 -0700

    Update AllHeadings.jsx

--- src/components/AllHeadings.jsx
+++ src/components/AllHeadings.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useParams,useNavigate } from 'react-router-dom';
 import { baseUrl } from '../utils/Base';
 import { toast } from 'react-toastify';
