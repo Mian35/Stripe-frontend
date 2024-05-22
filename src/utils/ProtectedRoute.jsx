fe6bc4cb48c65b09ee6bb85f45b2f0f08b57ecfc
Mian35 <74639641+Mian35@users.noreply.github.com>
2024-05-22 08:39:27 -0700

    Update ProtectedRoute.jsx

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import { Navigate, redirect,Outlet } from 'react-router-dom';
 import Login from '../components/Login';
 import { toast } from 'react-toastify';
